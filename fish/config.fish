f76d777223e724550e7e5a43811805c4d3105abf
Chad Nelson <cbn@mini.local>
2025-03-30 14:27:46 -0700

    Fish configs from work laptop

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,15 @@ source ~/.iterm2_shell_integration.(basename $SHELL)
 # Created by `userpath` on 2020-12-21 16:20:20
 set PATH $PATH /Users/chad/.local/bin
 set -g fish_user_paths "/usr/local/opt/node@14/bin" $fish_user_paths
+set -g fish_user_paths "/opt/homebrew/bin/" $fish_user_paths
+
+# Created by `pipx` on 2024-01-30 00:32:57
+set PATH $PATH /Users/local-chnelson/.local/bin
+
+
+set -x PATH $HOME/.rbenv/bin $PATH
+rbenv init - | source
+source ~/.cdl-ssm-util/session-fish-snippet.fish
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+#status --is-interactive; and source (pyenv virtualenv-init - |psub) 
